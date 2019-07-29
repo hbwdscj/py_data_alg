d97e3e944c02fbda33015b4627d93e1a37a22d82
zwjss <ss13806435635@163.com>
2019-07-29 11:23:22 +0800

    二分查找递归实现边界问题修正

--- BinarySearch_test.py
+++ BinarySearch_test.py
@@ -15,26 +15,33 @@ def binary_search(sorted_list, val):
             beg = mid - 1
     return -1
 """
-def binary_search_recursion(sorted_value, val):
-    beg = 0
-    end = len(sorted_value) - 1
-    while beg <= end:
-        mid = int((beg+end)/2)
-        if sorted_value[mid] == val:
-            return mid
-        elif sorted_value[mid] < val:
-            new_sorted_value = sorted_value[mid:end]
-            return binary_search_recursion(new_sorted_value, val)
-        else:
-            new_sorted_value = sorted_value[beg:mid]
-            return binary_search_recursion(new_sorted_value, val)
+import sys
+sys.setrecursionlimit(1000)
+def binary_search_recursion(sorted_value, beg, end, val):  
+    if beg >= end:
+        return -1
+    mid = int((beg+end)/2)
+    if sorted_value[mid] == val:
+        return mid
+    elif sorted_value[mid] < val:
+        return binary_search_recursion(sorted_value, mid+1, end, val)
+    else:
+        return binary_search_recursion(sorted_value, beg, mid, val)
+"""
 
+#错误记录：第一版实现出现错误是因为将sorted_value切片传入新的递归函数，导致序列的下标志
+#出现改变，因此返回的mid值不是最开始sorted_value的下标，循环部分代码如下:
+if sorted_value[mid] == val:
+    return mid
+elif sorted_value[mid] < val:
+    return binary_search_recursion(sorted_value[mid:end], val) 此处切片就会厂商也能够一个新的list，导致下标改变
+
+"""
 
 a = list(range(4))
-
-b1 = binary_search_recursion(a, 0)
-b2 = binary_search_recursion(a, 1)
-b3 = binary_search_recursion(a, 2)
-b4 = binary_search_recursion(a, 3)
-
-print("%s, %s, %s, %s," %(b1, b2, b3, b4))
+b1 = binary_search_recursion(a, 0, len(a), 0)
+b2 = binary_search_recursion(a, 0, len(a), 1)
+b3 = binary_search_recursion(a, 0, len(a), 2)
+b4 = binary_search_recursion(a, 0, len(a), 3)
+b5 = binary_search_recursion(a, 0, len(a), 10)
+print("%s, %s, %s, %s, %s" %(b1, b2, b3, b4, b5))
